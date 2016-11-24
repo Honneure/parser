92157919f11114c9c920a1cbc0f882f9c9c66485
Honneure <clement.dutartre@gmail.com>
2016-11-24 13:25:36 +0000

    Final commit

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express = require("express");
+var os = require("os");
 
 var app = express();
 var port = process.env.PORT || 8080;
 
+var infosystem = {
+    ipadress: "",
+    language: "",
+    software: ""
+    
+};
+
 app.get('/', function (req, res) {
-    res.send("Hello World");
+    infosystem.ipadress = req.connection.remoteAddress;
+    infosystem.language = req.headers["accept-language"];
+    infosystem.software = os.arch();
+    res.send(infosystem);
 });
 
 app.listen(port, function () {
     console.log("Server is running !");
 });
+
